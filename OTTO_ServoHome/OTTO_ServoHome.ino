8929bc2fe8f2b6716bff55c42effb6c85ec03b01
Alain Diart <adiart@studio-goliath.com>
2017-11-08 00:02:13 +0100

    Makes Otto_ServoHome Wemos D1 friendly

--- OTTO_ServoHome/OTTO_ServoHome.ino
+++ OTTO_ServoHome/OTTO_ServoHome.ino
@@ -1,27 +1,31 @@
 //----------------------------------------------------------------
 // Code to put all 4 servos in home position (90 degrees)
+//  - Adapted for Wemos D1
 //-----------------------------------------------------------------
 #include <Servo.h> 
 #include <EEPROM.h>
 #include <Otto.h>
+
 Otto Otto;  //This is Otto!
 
 //---------------------------------------------------------
 //-- First step: Configure the pins where the servos are attached
 /*
-         --------------- 
-        |     O   O     |
-        |---------------|
-YR 3==> |               | <== YL 2
-         --------------- 
-            ||     ||
-RR 5==>   -----   ------  <== RL 4
-         |-----   ------|
+          --------------- 
+         |     O   O     | o)) BZ D4
+         |---------------|
+YR D6==> |               | <== YL D5
+          --------------- 
+             ||     ||
+RR D8==>   -----   ------  <== RL D7
+          |-----   ------|
 */
-  #define PIN_YL 2 //servo[0]
-  #define PIN_YR 3 //servo[1]
-  #define PIN_RL 4 //servo[2]
-  #define PIN_RR 5 //servo[3]
+  #define PIN_YL D5       // servo[0] Body left
+  #define PIN_YR D6       // servo[1] Body right
+  #define PIN_RL D7       // servo[2] Foot left
+  #define PIN_RR D8       // servo[3] Foot right
+  #define NOISESENSOR -1  // Not used
+  #define BUZZER      D4  // Bzz bzz
 
 ///////////////////////////////////////////////////////////////////
 //-- Global Variables -------------------------------------------//
@@ -31,7 +35,9 @@ RR 5==>   -----   ------  <== RL 4
 ///////////////////////////////////////////////////////////////////
 void setup(){
   //Set the servo pins
-  Otto.init(PIN_YL,PIN_YR,PIN_RL,PIN_RR,true);
+  bool load_calibration = true;
+  Otto.init(PIN_YL,PIN_YR,PIN_RL,PIN_RR,load_calibration, NOISESENSOR, BUZZER);
+  Otto.sing(S_connection); //Otto wake up!
 }
 
 ///////////////////////////////////////////////////////////////////
