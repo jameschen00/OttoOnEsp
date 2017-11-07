a8c10dba8f86c3443dbaf7c187b6aa08557df245
Alain Diart <adiart@studio-goliath.com>
2017-11-07 23:49:51 +0100

    Because when Otto wakes up, it should always sing for us

--- Otto_easycal/Otto_easycal.ino
+++ Otto_easycal/Otto_easycal.ino
@@ -14,6 +14,8 @@
 #define YR_BODYRIGHT  D6
 #define RL_FOOTLEFT   D7
 #define RR_FOOTRIGHT  D8
+#define NOISESENSOR   -1
+#define BUZZER        D4
 
 Otto Otto;
 
@@ -22,18 +24,19 @@ void setup()
   Serial.begin(9600);
 
   delay(500);
-  
+
   Serial.println("");
   Serial.println("## Otto Easy Calibration #########################");
 
   Serial.println("Otto.init");
-  Otto.init(YL_BODYLEFT, YR_BODYRIGHT, RL_FOOTLEFT, RR_FOOTRIGHT, false);
+  bool load_calibration = false;
+  Otto.init(YL_BODYLEFT, YR_BODYRIGHT, RL_FOOTLEFT, RR_FOOTRIGHT, load_calibration, NOISESENSOR, BUZZER);
+  Otto.sing(S_connection); //Otto wake up!
 
   Serial.println("Otto.setTrims");
-  //Otto.setTrims(-7, -4, -4, 7); //change Trim "offset values" gradually until Otto is completely straight (90º)
-  Otto.setTrims(10, 5, -3, 0); // The one that worked for me
+  Otto.setTrims(-7, -4, -4, 7); //change Trim "offset values" gradually until Otto is completely straight (90º)
+
   // Otto.saveTrimsOnEEPROM(); //use only after completely straight(90º), delete this line after for further programming
-  
 }
 
 
@@ -41,6 +44,7 @@ void loop()  //test comparing before & after function
 {
   Serial.println("Otto.home");
   Otto.home();
+
   delay(4000);
   /*
   Otto.updown(6, 500, BIG);
